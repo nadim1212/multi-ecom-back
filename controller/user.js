72603dc3ea783da142156e237d639edb3f8b4bac
nadim1212 <nadimniloy.swe@gmail.com>
2023-05-29 14:36:14 +0600

    app.js

--- controller/user.js
+++ controller/user.js
@@ -40,7 +40,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://multi-eco.onrender.com/activation/${activationToken}`;
+    const activationUrl = `https://www.test.aliitltd.org/activation/${activationToken}`;
 
     try {
       await sendMail({
