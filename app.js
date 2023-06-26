e9fe84a6707f09d59636dc6f33d5af4c1d51cc88
nadim1212 <nadimniloy.swe@gmail.com>
2023-06-26 18:18:22 +0600

    app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'https://www.test.aliitltd.org',
+  origin: 'https://multi-eco.onrender.com/',
   credentials: true
 }));
 
